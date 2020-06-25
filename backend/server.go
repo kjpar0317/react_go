146056dfcabc868469d24b3177ccf47ff279327d
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-25 09:53:54 +0900

    설명 추가

--- backend/server.go
+++ backend/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 
@@ -11,6 +12,14 @@ import (
 func main() {
 	e := echo.New()
 
+	e.HideBanner = true
+
+	fmt.Println(">> 백앤드 서버 (휘발성)")
+	fmt.Println("만든 이유 : 백앤드를 전혀 모르는 프론트앤드 개발자 개인 연습용")
+	fmt.Println("주의점 : DB가 없기 때문에 휘발성임(서버 재기동 시 데이터 초기화)")
+	fmt.Println("라이센스 : 공개용라이선스 (개인 무료)")
+	fmt.Println("만든이 : 박광주(kjpar_kr@yahoo.co.kr)")
+
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -20,8 +29,6 @@ func main() {
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
 	}))
-
-	e.Logger.Info("프론트 개발자를 위한 설치 없는 백앤드 서버 (휘발성)")
 
 	// Login route
 	e.POST("/login", controllers.DoLogin)
@@ -41,5 +48,13 @@ func main() {
 
 	r.POST("/test", controllers.Restricted)
 
-	e.Logger.Fatal(e.Start(":8080"))
+	/*
+	  로그 포맷 참고
+	 https://echo.labstack.com/middleware/logger
+	*/
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${time_rfc3339} ${method} - ${uri} ${status}\n",
+	}))
+
+	e.Logger.Fatal(e.Start(":7080"))
 }
