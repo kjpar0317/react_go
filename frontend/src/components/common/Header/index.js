cc9a00152424f758ddfa9eaa2a5819870e626041
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-25 17:21:35 +0900

    헤더 수정

--- frontend/src/components/common/Header/index.js
+++ frontend/src/components/common/Header/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 // import './styles.css';
 
 const Header = () => {
+    let history = useHistory();
+
+    const doLogout = () => {
+        sessionStorage.clear();
+
+        // history.push("/");
+        history.go("/");
+    }
+
     return (
         <Navbar bg="light" expand="lg">
             <Navbar.Brand href="#home">React Study</Navbar.Brand>
@@ -17,6 +27,8 @@ const Header = () => {
                         className="mr-sm-2"
                     />
                     <Button variant="outline-success">Search</Button>
+                    &nbsp;
+                    <small><a onClick={doLogout}>logout</a></small>
                 </Form>
             </Navbar.Collapse>
         </Navbar>
