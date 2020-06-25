53757619b7b6beebe2781561a4da2982cd82db63
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-25 17:08:01 +0900

    테스트

--- frontend/src/features/Login/index.js
+++ frontend/src/features/Login/index.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card, Form, Button } from 'react-bootstrap'
 
-const Login = ({ changeLoginState }) => {
-    const doLogin = () => {
-        changeLoginState(true);
+import { doLogin } from '../../api/login'
+
+const Login = ({ changeUserid }) => {
+    const [ validated, setValidated ] = useState(false);
+    const [ userid, setUserid ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    const handleSubmit = (e) => {
+        const form = e.currentTarget;
+
+        if (form.checkValidity() === false) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+
+        e.preventDefault();
+
+        setValidated(true);
+
+        doLogin(userid, password).then(res => {
+            sessionStorage.setItem('token', res.token);
+            sessionStorage.setItem('userid', res.userinfo.userid);
+            sessionStorage.setItem('username', res.userinfo.username);
+            changeUserid(res.userinfo.userid);
+        }).catch(e => {
+            console.log(e);
+        })
     };
 
-    return <button onClick={doLogin}>로그인</button>;
+    return (
+        <Card  className="text-center">
+            <Card.Header>로그인</Card.Header>
+            <Card.Body >
+                <Form noValidate validated={validated.toString()} onSubmit={handleSubmit}>
+                    <Form.Group controlId="userid" className="text-left">
+                        <Form.Label>아이디</Form.Label>
+                        <Form.Control required type="text" value={userid} onChange={ (e) => setUserid(e.target.value) } placeholder="아이디를 입력하세요" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword" className="text-left">
+                        <Form.Label>패스워드</Form.Label>
+                        <Form.Control required type="password"value={password} onChange={ (e) => setPassword(e.target.value) } placeholder="패스워드를 입력하세요" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Card.Body>
+            <Card.Footer className="text-muted">@PKJ</Card.Footer>
+        </Card>
+    );
 };
 
 export default Login;
