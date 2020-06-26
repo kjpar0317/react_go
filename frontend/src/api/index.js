12741834b4f569382aff01b9c66ce6d7417cebee
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-26 13:34:09 +0900

    통신 제대로 되나 테스트

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,1 +1,18 @@
+const doTest = () => {
+    return fetch(
+        `${process.env.REACT_APP_BASE_URI}/api/test`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+            }
+        }
+    )
+        .then((res) => res.text())
+        .catch((err) => {
+            throw err;
+        });
+};
 
+export { doTest };
