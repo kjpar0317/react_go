b6230880e90338227987670d54c8e8d6c6b4ce77
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-26 12:46:59 +0900

    텍스트로 수정 중

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,17 +3,22 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 
 import { all } from 'redux-saga/effects';
+
+import { LOGIN, loginReducer } from '../features/Login/slice';
 import { UNSPLASH, unsplashReducer } from '../features/ImageGrid/slice';
+
+import { watchLogin } from '../features/Login/saga';
 import { watchUnsplash } from '../features/ImageGrid/saga';
 
 export const rootReducer = combineReducers({
+    [LOGIN]: loginReducer,
     [UNSPLASH]: unsplashReducer,
 });
 
 const sagaMiddleware = createSagaMiddleware();
 
 function* rootSaga() {
-    yield all([watchUnsplash()]);
+    yield all([watchLogin(), watchUnsplash()]);
 }
 
 const createStore = () => {
