bcf258019db3ab38f212237831cb20703cefe7b3
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-29 11:15:05 +0900

    텍스트로 수정

--- backend/controllers/Common.go
+++ backend/controllers/Common.go
@@ -7,6 +7,18 @@ import (
 	"net/http"
 )
 
+/**
+// query string으로 들어올때
+name := c.QueryParam("name")
+// url의 path variable 획득할때
+name := c.Param("name")
+// from submit을 통해 전달되는 파라메터 획득
+name := c.FormValue("name")
+// body json을 통한 파라메터 획득
+params := make(map[string]string)
+_ := c.Bind(&params)
+name = params["name"]
+ */
 func Accessible(c echo.Context) (err error) {
 	return c.String(http.StatusOK, "Accessible")
 }
