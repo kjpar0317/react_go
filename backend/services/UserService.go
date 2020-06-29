02a860b0762a9b9908c9c18b5344682aba561239
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-29 11:16:13 +0900

    텍스트로 수정

--- backend/services/UserService.go
+++ backend/services/UserService.go
@@ -4,15 +4,36 @@ import (
 	"../models"
 )
 
-func initUsers() [2]models.IUserinfo {
-	var userinfos [2]models.IUserinfo
+var userinfos [] models.IUserinfo
 
-	userinfos[0] = models.IUserinfo{"admin", "1234", "관리자", "스터디", true}
-	userinfos[1] = models.IUserinfo{"test", "1234", "테스터", "스터디", false}
+func InitUsers() [] models.IUserinfo {
+	userinfos = make([] models.IUserinfo, 2, 100)
+	userinfos = append(userinfos, models.IUserinfo{Userid: "admin", Password: "1234", Username: "관리자", Group: "스터디", Admin: true, Email: "kjpar_kr@yahoo.co.kr", HPhone: "000-1111-2222"})
+	userinfos = append(userinfos, models.IUserinfo{Userid: "test", Password: "1234", Username: "테스터", Group: "스터디", Email: "a@a.com", HPhone: "010-2222-3333"})
 
 	return userinfos
 }
 
-func New() [2]models.IUserinfo {
-	return initUsers()
+func SelectUser(userid string) models.IUserinfo {
+	for _, userinfo := range userinfos {
+		if userinfo.Userid == userid {
+			return userinfo
+		}
+	}
+	return models.IUserinfo {}
+}
+
+func CreateUser(userinfo models.IUserinfo) bool {
+	userinfos = append(userinfos, userinfo)
+
+	return true
+}
+
+func DeleteUser(userid string) bool {
+	for i, userinfo := range userinfos {
+		if userinfo.Userid == userid {
+			userinfos = append(userinfos[:i], userinfos[i+1:]...)
+		}
+	}
+	return true
 }
