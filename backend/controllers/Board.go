ab55d848c018de1d8d377d9f11a45761aa853d0f
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-30 13:27:45 +0900

    텍스트로 수정

---
+++ backend/controllers/Board.go
@@ -0,0 +1,53 @@
+package controllers
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+	"strconv"
+
+	"../models"
+	"../services"
+)
+
+func SelectBoardList(c echo.Context) (err error) {
+	filter := new(models.IBoardFilter)
+	if err = c.Bind(filter); err != nil {
+		return echo.ErrBadRequest
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"boardlist": services.SelectBoardList(*filter),
+	})
+}
+
+func AddBoard(c echo.Context) (err error) {
+	boardinfo := new(models.IBoard)
+
+	if err = c.Bind(boardinfo); err != nil {
+		return echo.ErrBadRequest
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"isRegisted": services.AddBoard(*boardinfo),
+	})
+}
+
+func EditBoard(c echo.Context) (err error) {
+	boardinfo := new(models.IBoard)
+
+	if err = c.Bind(boardinfo); err != nil {
+		return echo.ErrBadRequest
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"isRegisted": services.EditBoard(*boardinfo),
+	})
+}
+
+func DeleteBoard(c echo.Context) (err error) {
+	bbsno, _ := strconv.Atoi(c.Param("bbsno"))
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"isDeleted": services.DeleteBoard(bbsno),
+	})
+}
