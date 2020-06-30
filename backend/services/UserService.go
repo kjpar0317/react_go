84de91c7b8330522fa80bf9f55ec7f64512b345f
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-30 09:38:00 +0900

    텍스트로 수정

--- backend/services/UserService.go
+++ backend/services/UserService.go
@@ -7,7 +7,8 @@ import (
 var userinfos [] models.IUserinfo
 
 func InitUsers() [] models.IUserinfo {
-	userinfos = make([] models.IUserinfo, 2, 100)
+	// userinfos = [] models.IUserinfo {}
+	userinfos = make([] models.IUserinfo, 0, 100)
 	userinfos = append(userinfos, models.IUserinfo{Userid: "admin", Password: "1234", Username: "관리자", Group: "스터디", Admin: true, Email: "kjpar_kr@yahoo.co.kr", HPhone: "000-1111-2222"})
 	userinfos = append(userinfos, models.IUserinfo{Userid: "test", Password: "1234", Username: "테스터", Group: "스터디", Email: "a@a.com", HPhone: "010-2222-3333"})
 
@@ -24,16 +25,25 @@ func SelectUser(userid string) models.IUserinfo {
 }
 
 func CreateUser(userinfo models.IUserinfo) bool {
-	userinfos = append(userinfos, userinfo)
-
-	return true
+	// userid가 있는 경우 false
+	if SelectUser(userinfo.Userid) == (models.IUserinfo{}) {
+		userinfos = append(userinfos, userinfo)
+		return true
+	} else {
+		return false
+	}
 }
 
 func DeleteUser(userid string) bool {
-	for i, userinfo := range userinfos {
-		if userinfo.Userid == userid {
-			userinfos = append(userinfos[:i], userinfos[i+1:]...)
+	// userid가 있는 경우 false
+	if SelectUser(userid) == (models.IUserinfo{}) {
+		return false
+	} else {
+		for i, userinfo := range userinfos {
+			if userinfo.Userid == userid {
+				userinfos = append(userinfos[:i], userinfos[i+1:]...)
+			}
 		}
+		return true
 	}
-	return true
 }
