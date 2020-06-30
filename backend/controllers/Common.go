b4ff4ac6f293af82b6416d6e40e12215d2c7789d
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-06-30 09:38:39 +0900

    텍스트로 수정

--- backend/controllers/Common.go
+++ backend/controllers/Common.go
@@ -19,6 +19,18 @@ params := make(map[string]string)
 _ := c.Bind(&params)
 name = params["name"]
  */
+/**
+new 와 make 의 차이
+new(T):포인터를 반환하여 타입*T의 값으로 T를 입력하고, 메모리를 할당합니다. new(T)는 T{}와 같습니다. (초기화 X, 즉 제로 값이 되는것이다)
+make(T):T유형의 초기화된 값을 반환하고, 메모리를 할당하고 초기화합니다. 그것은 slice, map, channel에 사용된다.
+*/
+/*
+struct 비교
+session := &Session{}
+if (Session{}) == *session {
+    fmt.Println("session is empty")
+}
+*/
 func Accessible(c echo.Context) (err error) {
 	return c.String(http.StatusOK, "Accessible")
 }
