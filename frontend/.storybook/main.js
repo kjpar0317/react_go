eedcb143d0fc9f80d6b74f490c747daf1ffb5afe
kjpar0317 <kjpar_kr@yahoo.co.kr>
2020-07-23 12:44:40 +0900

    텍스트로 수정

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -2,8 +2,10 @@ module.exports = {
     stories: ['../src/components/**/*.stories.js'],
     addons: [
         '@storybook/preset-create-react-app',
-        '@storybook/addon-actions',
-        '@storybook/addon-links',
+        '@storybook/addon-links/register',
         '@storybook/addon-actions/register',
+        '@storybook/addon-knobs/register',
+        '@storybook/addon-notes/register',
+        '@storybook/addon-docs/react/preset'
     ],
 };
